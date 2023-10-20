5f54ec743581ca0b46754cb032e0c28a97b310cd
ffe4el <codkan20@gmail.com>
2023-10-20 10:03:08 +0900

    1020end

---
+++ IntentResult/app/src/main/java/com/example/intentresult/FriendActivity.java
@@ -0,0 +1,43 @@
+package com.example.intentresult;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class FriendActivity extends AppCompatActivity {
+    public static final String KEY_SIMPLE_MSG ="msg";
+
+    TextView textView;
+    EditText editText;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friend);
+
+        textView = findViewById(R.id.textView5);
+        editText = findViewById(R.id.editTextText2);
+        button = findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), FriendActivity.class);
+                String msg = editText.getText().toString();
+                intent.putExtra(KEY_SIMPLE_MSG, msg);
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+
+        Intent intent = getIntent();
+        String msg = intent.getStringExtra(KEY_SIMPLE_MSG);
+        textView.setText(msg);
+    }
+}
