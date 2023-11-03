1cd349d17fc8a27242b938109d458c855fe694fb
ffe4el <codkan20@gmail.com>
2023-11-03 11:00:09 +0900

    1103

---
+++ SQLiteDBSample/app/src/main/java/com/example/sqlitedbsample/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.sqlitedbsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText titleView;
+    EditText contentView;
+    Button addBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
